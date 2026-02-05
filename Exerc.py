3851a5bb87a79cee9759376c1fb4308443ace57e
Rafael Sousa <pereirafael96@gmail.com>
2026-02-05 06:03:58 +0000

    Adicionar script inicial com desafios em Python e configuração do workspace

---
+++ Exerc.py
@@ -0,0 +1,18 @@
+#Desafio 01-Crie um script Python que leia o nome de uma pessoa e mostra uma 
+#mensagem de boas-vindas de acordo com o valor digitado
+
+nome=input('Qual é o seu nome?')
+print('Seja bem-vindo(a),',nome)
+
+#Desafio 02 - Crie um script Python que leia o dia, o mês e o ano de nascimento de uma pessoa e mostra uma mensagem com a data formatada
+
+dia=input('Dia=')
+mes=input('Mês=')
+ano=input('Ano=')
+print('Você nasceu no dia',dia,'de',mes,'de',ano,'. Correto?')
+
+#Desafio 03 - Crie um script Python que leia dois números e tenta mostrar a soma entre eles.
+
+num1=input('Digite um número:')
+num2=input('Digite outro número:')
+print('A soma entre',num1,'e',num2,'é igual a',int(num1)+int(num2))
